d4812dfd805c386d544c96756f59227e89f61823
Archanach707 <54771334+Archanach707@users.noreply.github.com>
2022-06-30 22:51:49 +0530

    1. make slide right and left

--- js/index.js
+++ js/index.js
@@ -5,6 +5,29 @@ const rightBtn = document.getElementById('right')
 
 let activeSlide = 0
 
+rightBtn.addEventListener('click', () => {
+  activeSlide++
+
+  if(activeSlide > slides.length -1){
+    activeSlide = 0
+  }
+
+  setBgToBody()
+  setActiveSlide()
+})
+
+leftBtn.addEventListener('click', () => {
+  activeSlide--
+
+  if(activeSlide > slides.length -1){
+    activeSlide = 0
+  }
+
+  setBgToBody()
+  setActiveSlide()
+})
+
+
 setBgToBody()
 
 function setBgToBody() {
@@ -14,7 +37,7 @@ function setBgToBody() {
 function setActiveSlide() {
   slides.forEach(slide => slide.classList.remove('active'))
     slides[activeSlide].classList.add('active')
-    
+
 
 
 
