dba3774471bf20927bba97f233065166fa32aebb
Archanach707 <54771334+Archanach707@users.noreply.github.com>
2022-06-30 22:55:12 +0530

    1. project is complete background slider working smoothaly

--- js/index.js
+++ js/index.js
@@ -19,8 +19,8 @@ rightBtn.addEventListener('click', () => {
 leftBtn.addEventListener('click', () => {
   activeSlide--
 
-  if(activeSlide > slides.length -1){
-    activeSlide = 0
+  if(activeSlide < 0) {
+    activeSlide = slides.length -1
   }
 
   setBgToBody()
